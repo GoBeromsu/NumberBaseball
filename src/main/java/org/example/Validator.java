8ae686dbeaf376a9b962eadde1e145274c3914a9
goberomsu <gobeumsu@gmail.com>
2023-05-01 20:38:55 +0900

    Feat : getError
    만약에 입력 문자의 에러가 있다면 에러 메시지를 string으로 반환합니다
    
    앞으로의 투두리스트
    - 상태 분기에 따른 에러 메시지 다르게 할 것
    - getError는 모호하고 null을 반환하니 수정 할 것

--- src/main/java/org/example/Validator.java
+++ src/main/java/org/example/Validator.java
@@ -27,9 +27,16 @@ public class Validator {
         }
         return true;
     }
+    public String getError() {
+        if (!checkError()) {
+            return "Error: Invalid input. Please enter four unique numbers between 0 and 9.";
+        }
+        return null;
+    }
     public static void main(String[] args) {
         int[] testData = {1, 2, 3, 4};
         Validator validator = new Validator(testData);
         System.out.println("checkError : "+validator.checkError());
+        System.out.println("getError : "+validator.getError());
     }
 }
