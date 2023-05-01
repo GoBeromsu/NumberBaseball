f437e25d888ae0cfee67c4720afcb37833e4c1f2
goberomsu <gobeumsu@gmail.com>
2023-05-01 20:32:24 +0900

    feat : 에러 판단 메소드
    에러 판단 기준
    - 길이가 짧은 경우
    - 입력 값이 0~9를 벗어나는 경우
    - 중복이 있는 경우

---
+++ src/main/java/org/example/Validator.java
@@ -0,0 +1,35 @@
+package org.example;
+
+
+public class Validator {
+    private int[] data;
+
+    Validator(int[] data) {
+        this.data = data;
+    }
+    private boolean checkError (){
+//길이가 짧은 경우
+        if (data.length!=4){
+            return false;
+        }
+
+        for(int i=0;i<4;i++) {
+//입력 값이 0~9의 범위를 벗어나는 경우
+            if (data[i] < 0 || data[i] > 9) {
+                return false;
+            }
+// 중복 체크
+            for (int j = i + 1; j < 4; j++) {
+                if (data[i] == data[j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int[] testData = {1, 2, 3, 4};
+        Validator validator = new Validator(testData);
+        System.out.println("checkError : "+validator.checkError());
+    }
+}
