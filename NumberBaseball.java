ad576f9b8512a222cb04b05ccbe49a7c1a77c060
C0Zl <hyeee19@naver.com>
2023-05-01 20:49:49 +0900

    feat: Create variable CompNum
    컴퓨터 숫자 변수 생성

---
+++ NumberBaseball.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class CompNum {
+    public static void main(String[] args) {
+        Random random = new Random();
+        String[] compNum = new String[4];
+        for(int i=0; i<4; i++) { //중복제외 변수 생성
+            int n = random.nextInt(10);
+            compNum[i] = Integer.toString(n);
+            for(int j=0; j<i; j++) {
+                if(compNum[i].equals(compNum[j])) {
+                    i--;
+                }
+            }
+        }
+//        for (String s : compNum) {
+//            System.out.print(s);
+//        }S
+
+    }
+
+}
